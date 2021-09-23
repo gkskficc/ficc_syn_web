50d3dd720e066a50f504c60849cd9d6d8604b240
gkskficc <gkskficc@gmail.com>
2021-09-23 21:09:52 +0900

    first commit

---
+++ FICC_SYN_WEB_VERSION/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace FICC_SYN_WEB_VERSION.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
